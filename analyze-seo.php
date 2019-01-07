1611be73ea5263fd06485e3c57a5c35e614d3b4a
Holkan <luah613@gmail.com>
2019-01-07 12:47:56 -0600

    taxonomy seo fields

--- analyze-seo.php
+++ analyze-seo.php
@@ -20,6 +20,9 @@ define( 'SEO_ANALYSIS_URL', trailingslashit( plugins_url( '', __FILE__ ) ) );
 defined('ABSPATH') || exit;
 
 include_once SEO_ANALYSIS_PATH . '/includes/seo-metaboxes.php';
+include_once SEO_ANALYSIS_PATH . '/php/taxonomy_settings.php';
+
+
 
 /**
  * Enqueue front end and editor JavaScript and CSS
@@ -50,6 +53,185 @@ function analyze_seo_scripts() {
 add_action('enqueue_block_editor_assets', 'analyze_seo_scripts');
 
 
+
+/**
+ * Taxonomy SEO fields
+*/
+
+if(is_admin()){
+    add_action( 'admin_init', 'add_term_boxes', 10, 1);
+}
+
+/**
+ * Add the meta box to taxonomies with per-term fields enabled.
+ */
+function add_term_boxes() {
+    foreach ( Taxonomy_settings()->get_enabled_taxonomies() as $slug ) {
+        add_action( $slug . '_add_form_fields', 'add_term_meta_fields', 10, 2 );
+        add_action( $slug . '_edit_form','edit_term_meta_fields', 10, 2 );
+    }
+    add_action( 'created_term', 'save_term_fields', 10, 3 );
+    add_action( 'edited_term', 'save_term_fields', 10, 3 );
+}
+
+/**
+ * Helper to get the translated <noscript> text for the character count.
+ *
+ * @param  string $text The text to count.
+ * @return string The text to go between the <noscript> tags.
+ */
+function noscript_character_count( $text ) {
+    return sprintf( __( '%d (save changes to update)', 'analyze-seo' ), strlen( $text ) );
+}
+
+/**
+ * Helper to construct an option name for per-term SEO fields.
+ *
+ * @param  object $term The term object
+ * @return string The option name
+ */
+function get_term_option_name( $term ) {
+    return "analyze-seo-term-{$term->term_taxonomy_id}";
+}
+
+/**
+ * Display the SEO fields for adding a term.
+ *
+ * @param string $taxonomy The taxonomy slug.
+ */
+function add_term_meta_fields( $taxonomy ) {
+    wp_nonce_field( plugin_basename( __FILE__ ), 'wp-seo-nonce' );
+    ?>
+        <h3><?php echo esc_html( 'SEO Fields' ); ?></h3>
+        <div class="wp-seo-term-meta-fields">
+            <div class="form-field">
+                <label for="wp_seo_meta_title"><?php esc_html_e( 'Title Tag', 'analyze-seo' ); ?></label>
+                <input type="text" id="wp_seo_meta_title" name="seo_meta[title]" value="" size="96" />
+                <p>
+                    <?php esc_html_e( 'Title word count: ', 'analyze-seo' ); ?>
+                    <span class="title-character-count"></span>
+                    <noscript><?php echo esc_html( noscript_character_count( '' ) ); ?></noscript>
+                </p>
+            </div>
+            <div class="form-field">
+                <label for="wp_seo_meta_description"><?php esc_html_e( 'Meta Description', 'analyze-seo' ); ?></label>
+                <textarea id="wp_seo_meta_description" name="seo_meta[description]" rows="2" cols="96"></textarea>
+                <p>
+                    <?php esc_html_e( 'Description word count: ', 'analyze-seo' ); ?>
+                    <span class="description-character-count"></span>
+                    <noscript><?php echo esc_html( noscript_character_count( '' ) ); ?></noscript>
+                </p>
+            </div>
+            <div class="form-field">
+                <label for="wp_seo_meta_keywords"><?php esc_html_e( 'Meta Keywords', 'analyze-seo' ) ?></label>
+                <textarea id="wp_seo_meta_keywords" name="seo_meta[keywords]" rows="2" cols="96"></textarea>
+            </div>
+        </div>
+    <?php
+}
+
+/**
+ * Display the SEO fields for editing a term.
+ *
+ * @param  object $tag The term object
+ * @param  string $taxonomy The taxonomy slug
+ */
+function edit_term_meta_fields( $tag, $taxonomy ) {
+    $values = get_option( get_term_option_name( $tag ), array( 'title' => '', 'description' => '', 'keywords' => '', 'objective' => ''));
+
+    wp_nonce_field( plugin_basename( __FILE__ ), 'wp-seo-nonce' );
+    ?>
+        <h2><?php echo esc_html( 'SEO Fields' ); ?></h2>
+        <table class="form-table wp-seo-term-meta-fields">
+            <tbody>
+                <tr class="form-field">
+                    <th scope="row"><label for="wp_seo_meta_title"><?php esc_html_e( 'Title Tag', 'analyze-seo' ); ?></label></th>
+                    <td>
+                        <input type="text" id="wp_seo_meta_title" name="seo_meta[title]" value="<?php echo esc_attr( $title = $values['title'] ); ?>" size="96" />
+                        <p class="description">
+                            <?php esc_html_e( 'Title word count: ', 'analyze-seo' ); ?>
+                            <span class="title-character-count"></span>
+                            <noscript><?php echo esc_html( noscript_character_count( $title ) ); ?></noscript>
+                        </p>
+                    </td>
+                </tr>
+                <tr class="form-field">
+                    <th scope="row"><label for="wp_seo_meta_description"><?php esc_html_e( 'Meta Description', 'analyze-seo' ); ?></label></th>
+                    <td>
+                        <textarea id="wp_seo_meta_description" name="seo_meta[description]" rows="2" cols="96"><?php echo esc_textarea( $description = $values['description'] ); ?></textarea>
+                        <p class="description">
+                            <?php esc_html_e( 'Description word count: ', 'analyze-seo' ); ?>
+                            <span class="description-character-count"></span>
+                            <noscript><?php echo esc_html( noscript_character_count( $description ) ); ?></noscript>
+                        </p>
+                    <td>
+                </tr>
+                <tr class="form-field">
+                    <th scope="row"><label for="wp_seo_meta_keywords"><?php esc_html_e( 'Meta Keywords', 'analyze-seo' ) ?></label></th>
+                    <td><textarea id="wp_seo_meta_keywords" name="seo_meta[keywords]" rows="2" cols="96"><?php echo esc_textarea( $values['keywords'] ); ?></textarea></td>
+                </tr>
+            </tbody>
+        </table>
+    <?php
+}
+
+/**
+ * Save the SEO term values as an option.
+ *
+ * @see wp_unslash(), which the Settings API and update_post_meta()
+ *     otherwise handle.
+ *
+ * @param  int $term_id Term ID.
+ * @param  int $tt_id Term taxonomy ID.
+ * @param  string $taxonomy Taxonomy slug.
+ */
+function save_term_fields( $term_id, $tt_id, $taxonomy ) {
+    if ( ! isset( $_POST['taxonomy'] ) ) {
+        return;
+    }
+
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        return;
+    }
+
+    if ( !has_term_fields( $taxonomy ) ) {
+        return;
+    }
+
+    $object = get_taxonomy( $taxonomy );
+    if ( empty( $object->cap->edit_terms ) || ! current_user_can( $object->cap->edit_terms ) ) {
+        return;
+    }
+
+    if ( ! isset( $_POST['wp-seo-nonce'] ) ) {
+        return;
+    }
+
+    if ( ! wp_verify_nonce( $_POST['wp-seo-nonce'], plugin_basename( __FILE__ ) ) ) {
+        return;
+    }
+
+    if ( ! isset( $_POST['seo_meta'] ) ) {
+        $_POST['seo_meta'] = array();
+    }
+
+    foreach ( array( 'title', 'description', 'keywords' ) as $field ) {
+        $data[ $field ] = isset( $_POST['seo_meta'][ $field ] ) ? sanitize_text_field( wp_unslash( $_POST['seo_meta'][ $field ] ) ) : '';
+    }
+
+    $name = get_term_option_name( get_term( $term_id, $taxonomy ) );
+    if ( false === get_option( $name ) ) {
+        // Don't create an option unless at least one field exists.
+        $filtered_data = array_filter( $data );
+        if ( ! empty( $filtered_data ) ) {
+            add_option( $name, $data, null, false );
+        }
+    } else {
+        update_option( $name, $data );
+    }
+}
+
+
 /**
  * [currentpost_title_tag] Updates the title tag in single post
  * @param [string] $title
@@ -63,6 +245,8 @@ if(!function_exists('currentpost_title_tag')){
             $post_id = $post_object->ID;
             $title_tag = get_post_meta($post_id, 'title_tag', true);
             return $title_tag;
+        }else if(is_category() || is_tag() || is_tax() ){
+
         }
         return $title;
     }
---
+++ php/taxonomy_settings.php
@@ -0,0 +1,139 @@
+<?php
+class Taxonomy_settings{
+	/**
+	 * Instance of this class.
+	 *
+	 * @var object
+	 */
+	private static $instance = null;
+
+	/**
+	 * The default options to save.
+	 *
+	 * @var array.
+	 */
+	public $default_options = array();
+
+	/**
+	 * Storage unit for the current option values of the plugin.
+	 *
+	 * @var array.
+	 */
+	public $options = array();
+
+	/**
+	 * Taxonomies with archive pages, which can have meta fields set for them.
+	 *
+	 * @see  WP_SEO_Settings::setup().
+	 *
+	 * @var array Term objects.
+	 */
+	private $taxonomies = array();
+
+	/**
+	 * Post types that can be viewed individually and have per-entry meta values.
+	 *
+	 * @see  WP_SEO_Settings::setup().
+	 *
+	 * @var array Post type objects.
+	 */
+	private $single_post_types = array();
+
+	const SLUG = 'analyze-seo';
+
+	public static function instance(){
+		if(!isset(self::$instance)){
+			self::$instance = new Taxonomy_settings;
+			self::$instance->setup();
+		}
+		return self::$instance;
+	}
+
+	protected function setup(){
+		add_action( 'wp_loaded', array( $this, 'set_properties' ) );
+	}
+
+	public function set_properties(){
+		/**
+		 * Filter the taxonomies that support SEO fields on term archive pages.
+		 *
+		 * @param  array Associative array of taxonomy keys and objects.
+		 */
+		$this->taxonomies = apply_filters( 'wp_seo_taxonomies', wp_list_filter( get_taxonomies( array( 'public' => true ), 'objects' ), array( 'label' => false ), 'NOT' ) );
+
+		/**
+		 * Filter the options to save by default.
+		 *
+		 * These are also the settings shown when the option does not exist,
+		 * such as when the the plugin is first activated.
+		 *
+		 * @param  array Associative array of setting names and values.
+		 */
+		$this->default_options = apply_filters( 'wp_seo_default_options', array( 'post_types' => array_keys( $this->single_post_types ), 'taxonomies' => array_keys( $this->taxonomies ) ) );
+	}
+
+	/**
+	 * Set $options with the current database value.
+	 */
+	public function set_options() {
+		$this->options = get_option( $this::SLUG, $this->default_options );
+	}
+
+	/**
+	 * Get an option value.
+	 *
+	 * @param  string $key The option key sought.
+	 * @param  mixed $default Optional default.
+	 * @return mixed The value, or null on failure.
+	 */
+	public function get_option( $key, $default = null ) {
+		if ( empty( $this->options ) ) {
+			$this->set_options();
+		}
+		return isset( $this->options[ $key ] ) ? $this->options[ $key ] : $default;
+	}
+
+	/**
+	 * Get the $taxonomies property.
+	 *
+	 * @return array @see WP_SEO_Settings::taxonomies.
+	 */
+	public function get_taxonomies() {
+		return $this->taxonomies;
+	}
+
+	/**
+	 * Get the $single_post_types property.
+	 *
+	 * @return array @see WP_SEO_Settings::single_post_types.
+	 */
+	public function get_single_post_types() {
+		return $this->single_post_types;
+	}
+
+	/**
+	 * Get the taxonomies with per-term fields enabled.
+	 *
+	 * @return array With slugs of any enabled taxonomies.
+	 */
+	public function get_enabled_taxonomies() {
+		return $this->get_option( 'taxonomies', array() );
+	}
+
+	/**
+	 * Helper to check whether a taxonomy is set in "Add fields to individual."
+	 *
+	 * @param  string $taxonomy Taxonomy name
+	 * @return boolean
+	 */
+	public function has_term_fields( $taxonomy ) {
+		return in_array( $taxonomy, $this->get_enabled_taxonomies() );
+	}
+
+}//End Class
+
+function Taxonomy_settings(){
+	return Taxonomy_settings::instance();
+}
+add_action('after_setup_theme', 'Taxonomy_settings', 10, 1);
+// END CLASS HELPER
